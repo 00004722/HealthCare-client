973bf0081a6a2dd5a682defd016cc24ac59d2c84
David Mejia <dmejiaoliva@HP-Laptop-15-dw1xxx-9b4a043e.localdomain>
2023-12-03 09:11:18 -0600

    Style:images2

--- src/assets/components/Homepage/Sections/Section.jsx
+++ src/assets/components/Homepage/Sections/Section.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FaArrowRight } from "react-icons/fa";
 import { Link } from 'react-router-dom';
-import image from '../../assets/images/Homepage/second_section.png'; // Asegúrate de reemplazar './path/to/image.png' con la ruta correcta a tu imagen
+
 
 const Seccion = ({ title, paragraph, textB }) => {
 
