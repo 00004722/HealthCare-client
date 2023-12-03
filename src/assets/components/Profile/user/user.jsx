dde51f70b1442b95287432cea2cc60fdd7eb0f56
David Mejia <dmejiaoliva@HP-Laptop-15-dw1xxx-9b4a043e.localdomain>
2023-12-03 09:25:03 -0600

    Style:profile

--- src/assets/components/Profile/user/user.jsx
+++ src/assets/components/Profile/user/user.jsx
@@ -5,49 +5,9 @@ import UserCards from '../usercards/Usercards';
 import { FaUser } from 'react-icons/fa';
 
 const User = () => {
-    const [profileImage, setProfileImage] = useState(null);
-
-    const handleImageChange = (e) => {
-        // Manejar la selección de la imagen aquí
-        const file = e.target.files[0];
-        setProfileImage(file);
-    };
     return (
         <div className='font-font h-auto bg-fondocolor3 text-white' >
             <div className='flex flex-col gap-5 justify-items-center '>
-                {/* Section Username and email */}
-                <section className=' text-center p-5 '>
-                    <label htmlFor='profileImageInput' className='cursor-pointer'>
-                        {profileImage ? (
-                            <img
-                                src={URL.createObjectURL(profileImage)}
-                                alt='Profile'
-                                className='rounded-full w-32 h-32 mt-2 mx-auto'  // Ajusté w-32 y h-32 aquí
-                            />
-                        ) : (
-                            <>
-                                <FaUser size={64} color='#ccc' className='mx-auto w-32 h-32 mt-2 m-5' />
-                                <h2 className='font-semibold'>Agregar Foto de Perfil</h2>
-                            </>
-                        )}
-                        <input
-                            type='file'
-                            id='profileImageInput'
-                            accept='image/*'
-                            onChange={handleImageChange}
-                            style={{ display: 'none' }}
-                        />
-                    </label>
-
-
-                    <h2 className='font-semibold'>
-                        User Name {/* Conect username from DB */}
-                    </h2>
-                    <p>
-                        email {/* Conect email from DB */}
-                    </p>
-
-                </section>
                 {/* Section cards */}
                 <section className='flex flex-col items-center gap-5 m-5 '>
                     <h1 className='font-poppins  text-4xl text-center'>¿Ha cambiado</h1>
