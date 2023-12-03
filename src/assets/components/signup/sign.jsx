3ab41a9f3ccb68a51f151d0ce7e2ce0ea9235286
David Mejia <dmejiaoliva@HP-Laptop-15-dw1xxx-9b4a043e.localdomain>
2023-12-03 09:34:34 -0600

    Feature:signupe

--- src/assets/components/signup/sign.jsx
+++ src/assets/components/signup/sign.jsx
@@ -64,13 +64,10 @@ const Ingresar = ({ setIsLoggedIn }) => {
             });
 
             const { data } = await signUp(formData);
+            navigate('/');
+            setIsLoggedIn(true);
             console.log(data);
-            if (data) {
-                setIsLoggedIn(true);
-                sessionStorage.setItem('toke', data);
-                navigate('/')
-            }
-
+            sessionStorage.setItem('toke', data);
 
         }
     };
