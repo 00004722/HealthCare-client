565cf42dc2b6a56f927336cd7a75eabae5a98cb4
David Mejia <dmejiaoliva@HP-Laptop-15-dw1xxx-9b4a043e.localdomain>
2023-12-04 12:33:05 -0600

    Fix: Fotos

--- src/assets/components/Homepage/Homepage.jsx
+++ src/assets/components/Homepage/Homepage.jsx
@@ -15,12 +15,12 @@ const Homepage = () => {
         <Sections title={"Tu salud es importante!"}
           paragraph={"Nuestro equipo está comprometido en ofrecerte las herramientas y los recursos necesarios para que tomes el control de tu salud . Ya sea que estés buscando información sobre rutinas de ejercicio efectivas, consejos para mantener un peso saludable o cualquier otra consulta relacionada con tu bienestar."}
           textB={"LLEVAR EL CONTROL"}
-          image={imageSection1}
+          image={"imageSection1"}
           background={"bg-homeSection1"}
         />
 
         {/* Exercise section */}
-        <Seccion image={imageSection2}
+        <Seccion image={"imageSection2"}
           title={"Descubre una nueva rutina de ejercicios!"}
           paragraph={"En nuestro sitio, encontrarás una amplia variedad de entrenamientos. Ya sea que busques perder peso, ganar fuerza o simplemente mantenerte activo. ¡Empieza hoy mismo tu viaje hacia una vida más saludable y activa!"}
           textB={"DESCUBRELA"}
@@ -30,7 +30,7 @@ const Homepage = () => {
         <Sections title={"Descubrete!"}
           paragraph={"Con nuestra herramienta de cálculo, puedes determinar si te encuentras en tu peso adecuado y obtener tu índice de grasa corporal. Es fácil y rápido. Simplemente ingresa tus datos, y te proporcionaremos información  sobre tu estado de salud y composición corporal. "}
           textB={"REVISA LAS HERRAMIENTAS"}
-          image={imageSection3}
+          image={"imageSection3"}
           background={"bg-homeSection3"} />
 
         {/* Contact section */}
