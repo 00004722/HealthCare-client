e07fb9b77d4fcd075b1c45233c3c56c15df837fc
David Mejia <dmejiaoliva@HP-Laptop-15-dw1xxx-9b4a043e.localdomain>
2023-12-04 13:06:50 -0600

    Fix:sections images

--- src/assets/components/Homepage/Homepage.jsx
+++ src/assets/components/Homepage/Homepage.jsx
@@ -2,9 +2,7 @@ import React, { useState } from 'react';
 import FooterHP from './Sections/FooterHP'
 import Sections from './Sections/Sections'
 import Contact from './Sections/Contact'
-import image1 from "../../images/Homepage/first_section.jpg";
-import image2 from "../../images/Homepage/second_section.jpg";
-import image3 from "../../images/Homepage/third_section.jpg"
+
 
 import Seccion from './Sections/Section'
 const Homepage = () => {
@@ -17,12 +15,12 @@ const Homepage = () => {
         <Sections title={"Tu salud es importante!"}
           paragraph={"Nuestro equipo está comprometido en ofrecerte las herramientas y los recursos necesarios para que tomes el control de tu salud . Ya sea que estés buscando información sobre rutinas de ejercicio efectivas, consejos para mantener un peso saludable o cualquier otra consulta relacionada con tu bienestar."}
           textB={"LLEVAR EL CONTROL"}
-          image={image1}
+          image={"./src/assets/images/Homepage/first_section.png"}
           background={"bg-homeSection1"}
         />
 
         {/* Exercise section */}
-        <Seccion image={image2}
+        <Seccion image={"./src/assets/images/Homepage/second_section.png"}
           title={"Descubre una nueva rutina de ejercicios!"}
           paragraph={"En nuestro sitio, encontrarás una amplia variedad de entrenamientos. Ya sea que busques perder peso, ganar fuerza o simplemente mantenerte activo. ¡Empieza hoy mismo tu viaje hacia una vida más saludable y activa!"}
           textB={"DESCUBRELA"}
@@ -32,7 +30,7 @@ const Homepage = () => {
         <Sections title={"Descubrete!"}
           paragraph={"Con nuestra herramienta de cálculo, puedes determinar si te encuentras en tu peso adecuado y obtener tu índice de grasa corporal. Es fácil y rápido. Simplemente ingresa tus datos, y te proporcionaremos información  sobre tu estado de salud y composición corporal. "}
           textB={"REVISA LAS HERRAMIENTAS"}
-          image={image3}
+          image={"./src/assets/images/Homepage/third_section.jpg"}
           background={"bg-homeSection3"} />
 
         {/* Contact section */}
